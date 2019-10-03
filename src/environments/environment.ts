ec0b0034e38b389206be56269a3882fce34bf4a3
Meiyappan Meiyappan Meiyappan <mmeiyappanmeiyap@worldbankgroup.org>
2019-10-03 18:50:31 +0530

    oct-3

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBx-GG2ddiU5oYxUqgC3Q4YnqGFLztk1A4",
+    authDomain: "clientpanelprod-cb48e.firebaseapp.com",
+    databaseURL: "https://clientpanelprod-cb48e.firebaseio.com",
+    projectId: "clientpanelprod-cb48e",
+    storageBucket: "clientpanelprod-cb48e.appspot.com",
+    messagingSenderId: "255377642645",
+    appId: "1:255377642645:web:cfddbbc3c20af5b857379c",
+    measurementId: "G-8R7WJ5C64S"
+  }
 };
 
 /*
